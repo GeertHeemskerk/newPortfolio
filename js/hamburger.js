669ba17273373f7a46f15e9b020c75d44d73a2bf
GeertHeemskerk <leaguecrafter@gmail.com>
2019-05-10 12:58:33 +0200

    hamburger menu done, now make it beautiful

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,9 @@
 window.onload = function(){
   var hamburger = document.getElementsByClassName('hamburger')[0];
+  var header = document.getElementsByClassName('header')[0];
 
   hamburger.onclick = function(){
     hamburger.classList.toggle("open");
+    header.classList.toggle("open");
   }
 }

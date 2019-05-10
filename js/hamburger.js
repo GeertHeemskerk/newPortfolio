9a8c7618536cc015c850cc26a36ce4602d637498
GeertHeemskerk <leaguecrafter@gmail.com>
2019-05-10 16:26:11 +0200

    added animations and fixed hamburger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,14 @@
 window.onload = function(){
   var hamburger = document.getElementsByClassName('hamburger')[0];
   var header = document.getElementsByClassName('header')[0];
-
+  
   hamburger.onclick = function(){
     hamburger.classList.toggle("open");
     header.classList.toggle("open");
   }
+
+  var $svg = $('svg').drawsvg({
+       duration: 4000
+  });
+  $svg.drawsvg('animate');
 }

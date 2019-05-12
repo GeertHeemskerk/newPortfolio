f6f552669053acf42af9b726e548aa5133c9d6c0
GeertHeemskerk <leaguecrafter@gmail.com>
2019-05-12 22:32:41 +0200

    hamburger menu + animations done, drawSVG done, Min jq js added

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,14 +1,19 @@
-window.onload = function(){
+$(window).ready(function(){
   var hamburger = document.getElementsByClassName('hamburger')[0];
   var header = document.getElementsByClassName('header')[0];
-  
+
   hamburger.onclick = function(){
-    hamburger.classList.toggle("open");
-    header.classList.toggle("open");
+    if($(".hamburger").hasClass("open")){
+      $(".hamburger").removeClass("open");
+      $(".header").removeClass("open");
+      hamburger.classList.toggle("close");
+      header.classList.toggle("close");
+    }else{
+      $(".hamburger").removeClass("close");
+      $(".header").removeClass("close");
+      hamburger.classList.toggle("open");
+      header.classList.toggle("open");
+    }
   }
 
-  var $svg = $('svg').drawsvg({
-       duration: 4000
-  });
-  $svg.drawsvg('animate');
-}
+});
---
+++ js/svgAnimations.js
@@ -0,0 +1,27 @@
+$(window).ready(function(){
+  var firstTrigger = false;
+  var secondTrigger = false;
+
+  $(document).scroll(function(){
+    //console.log($(document).scrollTop());
+    if($(this).scrollTop() > 1800 && !firstTrigger){
+      var mySVG = $('#coding').drawsvg();
+      mySVG.drawsvg('animate');
+      $('path').css({
+        "fill" : "#646464",
+        "-webkit-animation": "opacity 9s"
+      });
+      firstTrigger = true;
+    }if($(this).scrollTop() > 2265 && !secondTrigger){
+      var mySVG = $('#designing').drawsvg();
+      mySVG.drawsvg('animate');
+      $('path').css({
+        "fill" : "#646464",
+        "-webkit-animation": "opacity 7s"
+      });
+      secondTrigger = true;
+    }
+  });
+  var mySVG = $('.logo').drawsvg();
+  mySVG.drawsvg('animate');
+});
